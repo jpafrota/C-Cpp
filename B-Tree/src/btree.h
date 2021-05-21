f140bc1c934b8e3a3b93271fb78d88faa969dec8
Jorge Frota <81825051+jpafrota@users.noreply.github.com>
2021-05-21 19:59:36 -0400

    definicao da struct

--- B-Tree/src/btree.h
+++ B-Tree/src/btree.h
@@ -1,3 +1,13 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#define M 5
+
+typedef struct btreenode BTreeNode;
+struct BTreeNode{
+
+    int chaves[M];
+    int tamanho;
+    BTreeNode *filhos[M];
+
+};
